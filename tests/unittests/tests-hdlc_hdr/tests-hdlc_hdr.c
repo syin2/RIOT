3bd968253718767022e58070c3dd70ea1651bd3c
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-03-03 17:45:30 +0100

    Unittest: PPP: Made a different unittest for HDLC FCS

--- tests/unittests/tests-hdlc_hdr/tests-hdlc_hdr.c
+++ tests/unittests/tests-hdlc_hdr/tests-hdlc_hdr.c
@@ -11,6 +11,7 @@
  *
  * @file
  */
+
 #include <errno.h>
 #include <stdlib.h>
 
@@ -20,13 +21,6 @@
 
 #include "unittests-constants.h"
 #include "tests-hdlc_hdr.h"
-
-#define PPPINITFCS16    0xffff  /* Initial FCS16 value */
-#define PPPGOODFCS16    0xf0b8  /* Good final FCS16 value */
-
-#define PPPINITFCS32  0xffffffff   /* Initial FCS32 value */
-#define PPPGOODFCS32  0xdebb20e3   /* Good final FCS32 value */
-
 
 
 static void test_hdlc_hdr_set_get_address(void)
@@ -50,50 +44,12 @@ static void test_hdlc_hdr_set_get_protocol(void)
 	TEST_ASSERT_EQUAL_INT(protocol, hdlc_hdr_get_protocol(&hdr));
 }
 
-//Adapted test from RFC1662
-static void test_hdlc_hdr_fcs_checksum(void)
-{
-	uint16_t trialfcs;
-	uint8_t cp[11] = "test_data\0\0";
-	int len = 9;
-
-	trialfcs = hdlc_fcs16( PPPINITFCS16, &cp[0], len );
-	trialfcs ^= 0xffff;                 /* complement */
-	cp[len] = (trialfcs & 0x00ff);      /* least significant byte first */
-	cp[len+1] = ((trialfcs >> 8) & 0x00ff);
-
-	trialfcs = hdlc_fcs16( PPPINITFCS16, &cp[0], len + 2 );
-
-	TEST_ASSERT_EQUAL_INT(PPPGOODFCS16, trialfcs);
-}
-static void test_hdlc_hdr_fcs32_checksum(void)
-{
-	uint32_t trialfcs;
-
-	uint8_t cp[13] = "test_data\0\0";
-	int len = 9;
-
-	/* add on output */
-	trialfcs = hdlc_fcs32( PPPINITFCS32, &cp[0], len );
-	trialfcs ^= 0xffffffff;             /* complement */
-	cp[len] = (trialfcs & 0x00ff);      /* least significant byte first */
-	cp[len+1] = ((trialfcs >>= 8) & 0x00ff);
-	cp[len+2] = ((trialfcs >>= 8) & 0x00ff);
-	cp[len+3] = ((trialfcs >> 8) & 0x00ff);
-
-	/* check on input */
-	trialfcs = hdlc_fcs32( PPPINITFCS32, &cp[0], len + 4 );
-	TEST_ASSERT_EQUAL_INT(PPPGOODFCS32, trialfcs);
-
-}
 
 Test *tests_hdlc_hdr_tests(void)
 {
     EMB_UNIT_TESTFIXTURES(fixtures) {
         new_TestFixture(test_hdlc_hdr_set_get_address),
         new_TestFixture(test_hdlc_hdr_set_get_protocol),
-        new_TestFixture(test_hdlc_hdr_fcs_checksum),
-        new_TestFixture(test_hdlc_hdr_fcs32_checksum),
     };
 
     EMB_UNIT_TESTCALLER(hdlc_hdr_tests, NULL, NULL, fixtures);
