f13a433d8ff0bd4c013b3cc63986360068e89736
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-03-03 17:27:33 +0100

    PPP: FCS is not a part of the header. Removed

--- sys/include/net/hdlc/hdr.h
+++ sys/include/net/hdlc/hdr.h
@@ -12,7 +12,6 @@ extern "C" {
 typedef struct __attribute__((packed)) {
 	uint8_t address; //Address
 	uint16_t protocol; //protocol
-	network_uint16_t fcs; //Frame Check Sequence
 } hdlc_hdr_t;
 
 static inline void hdlc_hdr_set_address(hdlc_hdr_t *hdr, uint8_t address)
@@ -34,17 +33,6 @@ static inline uint16_t hdlc_hdr_get_protocol(hdlc_hdr_t *hdr)
 {
 	return hdr->protocol;
 }
-
-static inline void hdlc_hdr_set_fcs(hdlc_hdr_t *hdr, uint16_t fcs)
-{
-	hdr->fcs.u16 = fcs;
-} 
-
-static inline network_uint16_t hdlc_hdr_get_fcs(hdlc_hdr_t *hdr)
-{
-	return hdr->fcs;
-}
-
 
 uint16_t hdlc_fcs16(uint16_t, uint8_t*, int);
 uint32_t hdlc_fcs32(uint32_t, uint8_t*, int);
