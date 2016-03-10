8eeb2d7cbd01f2b30e548302de0ef53012725517
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-03-10 14:45:51 +0100

    Unittest: gnrc_ppp: Added tests for gnrc_ppp

---
+++ tests/unittests/tests-gnrc_ppp/tests-gnrc_ppp.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2014 José Ignacio Alamos <jialamos@uc.cl>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @{
+ *
+ * @file
+ */
+
+#include <errno.h>
+#include <stdlib.h>
+
+#include "embUnit.h"
+
+#include "unittests-constants.h"
+#include "tests-gnrc_ppp.h"
+
+
+static void test_gnrc_ppp_lcp_recv_pkt(void)
+{
+	/*TEST_ASSERT_EQUAL_INT(0xFF, hdr.address);*/
+}
+
+Test *tests_gnrc_ppp_tests(void)
+{
+    EMB_UNIT_TESTFIXTURES(fixtures) {
+        new_TestFixture(test_gnrc_ppp_lcp_recv_pkt),
+    };
+
+    EMB_UNIT_TESTCALLER(gnrc_ppp_tests, NULL, NULL, fixtures);
+
+    return (Test *)&gnrc_ppp_tests;
+}
+
+void tests_gnrc_ppp(void)
+{
+    TESTS_RUN(tests_gnrc_ppp_tests());
+}
+/** @} */
---
+++ tests/unittests/tests-gnrc_ppp/tests-gnrc_ppp.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2015 José Ignacio Alamos
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @addtogroup  unittests
+ * @{
+ *
+ * @file
+ * @brief       Unittests for the ``gnrc_ppp`` module
+ *
+ * @author      José Ignacio Alamos <jialamos@uc.cl>
+ */
+#ifndef TESTS_PPP_HDR_H_
+#define TESTS_PPP_HDR_H_
+
+#include "embUnit.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief   The entry point of this test suite.
+ */
+void tests_gnrc_ppp(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TESTS_PPP_HDR_H_ */
+/** @} */
+
