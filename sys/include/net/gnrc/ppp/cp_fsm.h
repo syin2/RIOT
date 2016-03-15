6cca4818ec50aee80675973eaea3f675764ed0e4
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-03-15 18:35:07 +0100

    gnrc_ppp: changed l_lcp to cp in each line

--- sys/include/net/gnrc/ppp/cp_fsm.h
+++ sys/include/net/gnrc/ppp/cp_fsm.h
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2015 Martine Lenders <mlenders@inf.fu-berlin.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    net_gnrc_slip SLIP
+ * @ingroup     net_gnrc
+ * @brief       Provides a SLIP interface over UART utilizing
+ *              @ref drivers_periph_uart.
+ * @{
+ *
+ * @file
+ * @brief       SLIP interface defintion
+ *
+ * @author      Martine Lenders <mlenders@inf.fu-berlin.de>
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ */
 
 #ifndef GNRC_PPP_FSM_H
 #define GNRC_PPP_FSM_H
--- sys/net/gnrc/link_layer/ppp/lcp/ppp_lcp.c
+++ sys/net/gnrc/link_layer/ppp/lcp/ppp_lcp.c
@@ -28,46 +28,46 @@
 #endif
 
 
-static void _tlu(ppp_ctrl_prot_t *l_lcp)
+static void _tlu(ppp_cp_t *cp)
 {
-	l_lcp->l_upper_msg |= PPP_MSG_UP;
+	cp->l_upper_msg |= PPP_MSG_UP;
 }
 
-static void _tld(ppp_ctrl_prot_t *l_lcp)
+static void _tld(ppp_cp_t *cp)
 {
-	l_lcp->l_upper_msg |= PPP_MSG_DOWN;
+	cp->l_upper_msg |= PPP_MSG_DOWN;
 }
 
-static void _tls(ppp_ctrl_prot_t *l_lcp)
+static void _tls(ppp_cp_t *cp)
 {
-	l_lcp->l_lower_msg |= PPP_MSG_UP;
+	cp->l_lower_msg |= PPP_MSG_UP;
 }
 
-static void _tlf(ppp_ctrl_prot_t *l_lcp)
+static void _tlf(ppp_cp_t *cp)
 {
-	l_lcp->l_lower_msg |= PPP_MSG_DOWN;
+	cp->l_lower_msg |= PPP_MSG_DOWN;
 }
 
-static void _irc(ppp_ctrl_prot_t *l_lcp)
+static void _irc(ppp_cp_t *cp)
 {
 	/*Depending on the state, set the right value for restart counter*/
 	/* TODO: Activate restart timer with corresponding time out */
-	switch(l_lcp->timer_select)
+	switch(cp->timer_select)
 	{
 		case RC_SEL_CONF:
-			l_lcp->counter_term = PPP_MAX_TERMINATE;
+			cp->counter_term = PPP_MAX_TERMINATE;
 			break;
 		case RC_SEL_TERM:
-			l_lcp->counter_conf = PPP_MAX_CONFIG;
+			cp->counter_conf = PPP_MAX_CONFIG;
 			break;
 		default:
 			/* Shouldn't be here */
 			break;
 	}
 }
-static void _zrc(ppp_ctrl_prot_t *l_lcp)
+static void _zrc(ppp_cp_t *cp)
 {
-	l_lcp->restart_counter = 0;
+	cp->restart_counter = 0;
 	/* Set timer to appropiate value TODO*/
 }
 
@@ -85,28 +85,28 @@ static void _src(ppp_cp_t *cp)
 	send_cp(cp, &pkt);
 	/* TODO: Set timeout for SRC */
 }
-static void _sca(ppp_ctrl_prot_t *l_lcp, cp_pkt_t *pkt)
+static void _sca(ppp_cp_t *cp, cp_pkt_t *pkt)
 {
 	pkt->hdr->code = PPP_CP_REQUEST_ACK;
-	send_cp(l_lcp, pkt);
+	send_cp(cp, pkt);
 }
-static void _scn(ppp_ctrl_prot_t *l_lcp, cp_pkt_t *pkt)
+static void _scn(ppp_cp_t *cp, cp_pkt_t *pkt)
 {
 	/* Check the content of received options */
 	if(pkt->opts->content_flag & OPT_HAS_REJ)
 	{
 		_remove_opts_by_status(PPP_CP_REQUEST_REJ, pkt->opts);
 		pkt->hdr->code = PPP_CP_REQUEST_REJ;
-		send_cp(l_lcp, pkt);
+		send_cp(cp, pkt);
 	}
 	else
 	{
 		_remove_opts_by_status(PPP_CP_REQUEST_NAK, pkt->opts);
 		pkt->hdr->code = PPP_CP_REQUEST_NAK;
-		send_cp(l_lcp, pkt);
+		send_cp(cp, pkt);
 	}
 }
-static void _str(ppp_ctrl_prot_t *l_lcp)
+static void _str(ppp_cp_t *cp)
 {
 	int id = 666; /*TODO*/
 	cp_pkt_t pkt;
@@ -114,9 +114,9 @@ static void _str(ppp_ctrl_prot_t *l_lcp)
 	pkt->hdr->id = id;
 	pkt->hdr->length = 4;
 	pkt->opts->num_opts = 0;
-	send_cp(l_lcp, pkt);
+	send_cp(cp, pkt);
 }
-static void _sta(ppp_ctrl_prot_t *l_lcp)
+static void _sta(ppp_cp_t *cp)
 {
 	int id = 666; /*TODO*/
 	cp_pkt_t pkt;
@@ -124,55 +124,55 @@ static void _sta(ppp_ctrl_prot_t *l_lcp)
 	pkt->hdr->id = id;
 	pkt->hdr->length = 4;
 	pkt->opts->num_opts = 0;
-	send_cp(l_lcp, pkt);
+	send_cp(cp, pkt);
 }
-static void _scj(ppp_ctrl_prot_t *l_lcp)
+static void _scj(ppp_cp_t *cp)
 {
-	send_cp(l_lcp, PPP_CP_CODE_REJ);
+	send_cp(cp, PPP_CP_CODE_REJ);
 }
-static void _ser(ppp_ctrl_prot_t *l_lcp)
+static void _ser(ppp_cp_t *cp)
 {
-	send_cp(l_lcp,PPP_CP_SER);
+	send_cp(cp,PPP_CP_SER);
 }
 
 
 /* Call functions depending on function flag*/
-static void _event_action(ppp_ctrl_prot_t *l_lcp) 
+static void _event_action(ppp_cp_t *cp) 
 {
 	uint8_t flags;
 	/* Reset link status */
-	l_lcp->l_upper_msg = 0;
-	l_lcp->l_lower_msg = 0;
+	cp->l_upper_msg = 0;
+	cp->l_lower_msg = 0;
 
-	flags = actions[l_lcp->state][l_lcp->event];
+	flags = actions[cp->state][cp->event];
 
-	if(flags & F_TLU) _tlu(l_lcp);
-	if(flags & F_TLD) _tld(l_lcp);
-	if(flags & F_TLS) _tls(l_lcp);
-	if(flags & F_TLF) _tlf(l_lcp);
-	if(flags & F_IRC) _irc(l_lcp);
-	if(flags & F_ZRC) _zrc(l_lcp);
-	if(flags & F_SRC) _src(l_lcp);
-	if(flags & F_SCA) _sca(l_lcp);
-	if(flags & F_SCN) _scn(l_lcp);
-	if(flags & F_STR) _str(l_lcp);
-	if(flags & F_STA) _sta(l_lcp);
-	if(flags & F_SCJ) _scj(l_lcp);
-	if(flags & F_SER) _ser(l_lcp);
+	if(flags & F_TLU) _tlu(cp);
+	if(flags & F_TLD) _tld(cp);
+	if(flags & F_TLS) _tls(cp);
+	if(flags & F_TLF) _tlf(cp);
+	if(flags & F_IRC) _irc(cp);
+	if(flags & F_ZRC) _zrc(cp);
+	if(flags & F_SRC) _src(cp);
+	if(flags & F_SCA) _sca(cp);
+	if(flags & F_SCN) _scn(cp);
+	if(flags & F_STR) _str(cp);
+	if(flags & F_STA) _sta(cp);
+	if(flags & F_SCJ) _scj(cp);
+	if(flags & F_SER) _ser(cp);
 }
 
-static int lcp_fsm(ppp_ctrl_prot_t *l_lcp)
+static int lcp_fsm(ppp_cp_t *cp)
 {
 	int8_t next_state;
 
 	/* Call the appropiate functions for each state */
-	_event_action(l_lcp);
+	_event_action(cp);
 	/* Set next state */
-	next_state = state_trans[l_lcp->state][l_lcp->event];
+	next_state = state_trans[cp->state][cp->event];
 
 	/* Keep in same state if there's something wrong (RFC 1661) */
 	if(next_state != S_UNDEF){
-		l_lcp->state = next_state;
+		cp->state = next_state;
 	}
 	return 0; /*TODO*/
 }
