c16f79077bec5a88242c5264a9ec15cf9f725ad1
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-03-15 13:38:02 +0100

    Sys: Makefile edited for adding ppp_pkt

--- sys/net/link_layer/ppp/pkt.c
+++ sys/net/link_layer/ppp/pkt.c
@@ -20,15 +20,15 @@
 #include <errno.h>
 #include <string.h>
 
-#include "net/pkt/pkt.h"
+#include "net/ppp/pkt.h"
 
 
 
 /*TODO return error if populate went bad */
 int ppp_pkt_populate(uint8_t *data, size_t length, cp_pkt_t *cp_pkt)
 {
-	cp_hdr_t hdr = (cp_hdr_t*) pkt->data;
-	cp_pkt->hdr = &cp_data;
+	cp_hdr_t hdr = (cp_hdr_t*) data;
+	cp_pkt->hdr = &hdr;
 
 	if (hdr->length != length) {
 		return EBADMSG;
