6742e58347f5715b1b8cdc26e90658a2a9d8b507
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-03-15 15:53:56 +0100

    PPP: pkt: Fixed byteorder issue

--- sys/include/net/ppp/pkt.h
+++ sys/include/net/ppp/pkt.h
@@ -6,6 +6,7 @@
 
 #include "net/gnrc.h"
 #include "net/gnrc/netdev2.h"
+#include "byteorder.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -16,18 +17,27 @@ extern "C" {
 typedef struct __attribute__((packed)){
 	uint8_t code;
 	uint8_t id;
-	uint16_t length;
+	network_uint16_t length;
 } cp_hdr_t;
 
 /* A PPP packet*/
 typedef struct __attribute__((packed))
 {
 	cp_hdr_t *hdr;
-	void *payload; 
+	uint8_t *payload; 
 } cp_pkt_t;
 
 
 int ppp_pkt_populate(uint8_t *data, size_t length, cp_pkt_t *cp_pkt);
+
+uint8_t ppp_pkt_get_code(cp_pkt_t *cp_pkt);
+uint8_t ppp_pkt_set_code(cp_pkt_t *cp_pkt, uint8_t code);
+uint8_t ppp_pkt_get_id(cp_pkt_t *cp_pkt);
+uint8_t ppp_pkt_set_id(cp_pkt_t *cp_pkt, uint8_t id);
+uint16_t ppp_pkt_get_length(cp_pkt_t *cp_pkt);
+uint16_t ppp_pkt_set_length(cp_pkt_t *cp_pkt, uint16_t length);
+uint8_t ppp_pkt_get_payload(cp_pkt_t *cp_pkt);
+uint8_t ppp_pkt_set_payload(cp_pkt_t *cp_pkt, void *payload);
 
 #ifdef __cplusplus
 }
--- tests/unittests/tests-ppp_pkt/tests-ppp_pkt.c
+++ tests/unittests/tests-ppp_pkt/tests-ppp_pkt.c
@@ -28,14 +28,14 @@ static void test_ppp_pkt_populate(void)
 	uint8_t code = 0x01;
 	uint8_t id = 33;
 	uint16_t length = 8;
-	uint8_t pkt[8] = {code,id,0,length,0x01,0x04,0x00,0x01};
+	uint8_t pkt[8] = {code,id,0x00,length,0x01,0x04,0x00,0x01};
 	cp_pkt_t cp_pkt;
 
 	ppp_pkt_populate(pkt, 8, &cp_pkt);
 
 	TEST_ASSERT_EQUAL_INT(code, cp_pkt.hdr->code);
 	TEST_ASSERT_EQUAL_INT(id, cp_pkt.hdr->id);
-	TEST_ASSERT_EQUAL_INT(length, cp_pkt.hdr->length);
+	TEST_ASSERT_EQUAL_INT(length, byteorder_ntohs(cp_pkt.hdr->length));
 	
 	TEST_ASSERT_EQUAL_INT(0,memcmp(pkt+4,cp_pkt.payload,4));
 }
