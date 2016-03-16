87ae42e1320f4f707e92232724f973b0d61c7278
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-03-16 11:51:58 +0100

    Temporal saving for switching branch

--- sys/include/net/gnrc/ppp/cp.h
+++ sys/include/net/gnrc/ppp/cp.h
@@ -154,3 +154,4 @@ typedef struct ppp_cp_t{
 int ppp_cp_populate_options(opt_stack *o_stack, uint8_t *payload, size_t p_size);
 int ppp_cp_opts_are_equal(cp_opt_t *o1, cp_opt_t *o2);
 int ppp_cp_optstacks_are_equal(opt_stack_t *s1, opt_stack_t *s2);
+void handle_cp_pkt(ppp_cp_t *cp, cp_pkt_t *pkt);
--- sys/net/gnrc/link_layer/ppp/cp.c
+++ sys/net/gnrc/link_layer/ppp/cp.c
@@ -36,50 +36,6 @@
 #include <inttypes.h>
 #endif
 
-
-
-/*Send Control Protocol. Assumes the opt payload is loaded in HDLC Control Protocol Buffer. */
-static int send_cp(ppp_ctrl_prot_t  *cp, cp_pkt_t *pkt)
-{
-	ppp_dev_t *dev = cp->dev;
-
-	/* Set code, identifier, length*/
-	dev->_payload_buf[0] = code;
-	dev->_payload_buf[1] = identifier;
-	uint32_t length;
-	/* if size is not zero, the hdlc cp buffer was preloaded */
-	/*TODO: Change number to labels*/
-	uint16_t cursor;
-	/* Generate payload with corresponding options */
-	cursor = 0;
-	cp_opt_t *copt;
-	for(int i=0;i<l_lcp->_num_opt;i++)
-	{
-		copt = &(opt_stack->_opt_buf[i]);
-		*(dst+cursor) = copt->type;
-		*(dst+cursor+1) = copt->p_size+2;
-		for(int j=0;j<copt->opt_size;j++)
-		{
-			*(dst+cursor+2+i) = copt->payload[j];
-		}
-		cursor += copt->opt_size+2;
-	}
-	length =  opt_size+4;
-	dev->_payload_buf[2] = length & 0xFF00;
-	dev->_payload_buf[3] = length & 0x00FF;
-
-	/* Create pkt snip */
-	gnrc_pktsnip_t *pkt = gnrc_pktbuf_add(NULL, dev->_payload_buf, length, GNRC_NETTYPE_UNDEF);
-	if (pkt == NULL){
-		DEBUG("PPP: not enough space in pkt buffer");
-		return 0; /*TODO Fix*/
-	}
-	
-	/* Send pkt to ppp_send*/
-	ppp_send(dev, pkt);
-	return 0; /*TODO*/
-}
-
 int ppp_cp_optchain_are_equal(cp_opt_t *o1, cp_opt_t *o2)
 {
 	if (o1->type != o2->type || o1->status != o2->status || o1->p_size != o2->p_size || memcmp(o1->payload,
@@ -205,7 +161,7 @@ static int _handle_cp_code_rej(ppp_cp_t *cp, gnrc_pktsnip_t *pkt)
 	cp->event = E_RXJm;
 }
 #endif
-static void _handle_cp_pkt(ppp_cp_t *cp, cp_pkt_t *pkt)
+void handle_cp_pkt(ppp_cp_t *cp, cp_pkt_t *pkt)
 {
 	cp->metadata.pkt = pkt;
 	/*LCP type*/
