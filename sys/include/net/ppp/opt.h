1cdf1dee693836d226a3b791d55a87ea7c3a0d8a
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-03-21 15:23:44 +0100

    PPP: Implemented ppp_opts_next

--- sys/include/net/ppp/opt.h
+++ sys/include/net/ppp/opt.h
@@ -19,6 +19,8 @@ typedef struct opt_metadada_t
 	int num;
 	void *head;
 	void *current;
+
+	int _co;
 } opt_metadata_t;
 
 static inline int _get_num_opt(void *head_opt, uint16_t opts_length)
@@ -39,14 +41,18 @@ static inline int _get_num_opt(void *head_opt, uint16_t opts_length)
 		p += *p;
 		num += 1;
 	}
-
+	
 	return num;
 }
 static inline int ppp_opts_init(opt_metadata_t *opt_metadata, cp_pkt_t *pkt)
 {
-	opt_metadata->head = opt_metadata->current = payload	
-	opt_metadata->num = 0;
-
+	opt_metadata->head = opt_metadata->current = pkt->payload;
+	opt_metadata->_co = 0;
+	int num = _get_num_opt(pkt->payload, pkt->length-sizeof(cp_hdr_t));
+	if (num == -EBADMSG)
+		return -1;
+	
+	opt_metadata->num = num;
 	return 0;
 }
 static inline void *ppp_opts_get_head(opt_metadata_t *opt_metadata)
@@ -59,7 +65,14 @@ static inline void ppp_opts_reset(opt_metadata_t *opt_metadata)
 }
 void *ppp_opts_next(opt_metadata_t *opt_metadata)
 {
-	
+	void *current = opt_metadata->current;
+	uint8_t opt_size = *(current+1);
+
+	if(opt_metadata_t->_co < num)
+	{
+		return (void*) (current+(int) opt_size);
+	}
+	return NULL;
 }
 
 static inline uint8_t ppp_opt_get_type(void *opt)
