474ed6ef8a33ab9a26b967688bee0b00acaf5889
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-03-21 16:01:15 +0100

    unittest: ppp: opt: added test_ppp_opts_init

--- tests/unittests/tests-ppp_pkt/tests-ppp_pkt.c
+++ tests/unittests/tests-ppp_pkt/tests-ppp_pkt.c
@@ -140,6 +140,23 @@ static void test_ppp_opts_init(void)
 	TEST_ASSERT_EQUAL_INT(-1, status);
 }
 
+static void test_ppp_opts_get_head(void)
+{
+	/* |--ConfigureReq--|--Identifier--|--Length(MSB)--|--Length(LSB)--|--Type--|--Length--|--MRU(MSB)--|--MRU(LSB)--| */
+	uint8_t code = 0x01;
+	uint8_t id = 33;
+	uint16_t length = 20;
+	uint8_t pkt[20] = {code,id,0x00,length,0x01,0x04,0x00,0x01,0x02,0x04,0x02,0x01,0x03,0x04,0x00,0x01,0x04,0x04,0xF0,0x01};
+	cp_pkt_t cp_pkt;
+	ppp_pkt_init(pkt, 20, &cp_pkt);
+
+	opt_metadata_t opt_metadata;
+	int status = ppp_opts_init(&opt_metadata, &cp_pkt);
+
+	/* Get head */
+	TEST_ASSERT_EQUAL_INT(1, ppp_opts_get_head(&opt_metadata) == pkt+4);
+}
+
 
 Test *tests_ppp_pkt_tests(void)
 {
@@ -150,6 +167,7 @@ Test *tests_ppp_pkt_tests(void)
         new_TestFixture(test_ppp_pkt_get_set_length),
         new_TestFixture(test_ppp_pkt_get_set_payload),
         new_TestFixture(test_ppp_opts_init),
+        new_TestFixture(test_opts_get_head),
     };
 
     EMB_UNIT_TESTCALLER(ppp_pkt_tests, NULL, NULL, fixtures);
