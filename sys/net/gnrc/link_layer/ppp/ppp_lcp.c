3e21b997a11128a7ab211587d7d8e86534f5044a
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-03-22 20:18:56 +0100

    gnrc_ppp: Changed abstraction. Moved functions from generic to lcp

--- sys/net/gnrc/link_layer/ppp/ppp_lcp.c
+++ sys/net/gnrc/link_layer/ppp/ppp_lcp.c
@@ -29,6 +29,7 @@
 #include <inttypes.h>
 #endif
 
+#if 0
 /* Negotiate local LCP options with NAK opts sent by peer */
 void lcp_negotiate_nak(void *lcp_opt, cp_pkt_metadata_t *metadata)
 {
@@ -68,3 +69,83 @@ void lcp_negotiate_nak(void *lcp_opt, cp_pkt_metadata_t *metadata)
 		curr_opt = ppp_opts_next(opts_handler);
 	}
 }
+#endif
+
+static int _handle_cp_rcr(ppp_cp_t *cp)
+{
+	if (cp->metadata.opts_status_content  & (OPT_HAS_NAK | OPT_HAS_REJ))
+	{
+		cp->event = E_RCRm;
+	}
+	else
+	{
+		cp->event = E_RCRp;
+	}
+
+return 0; /*TODO: Fix output*/
+}
+static int _handle_cp_rca(ppp_cp_t *cp)
+{
+	cp_pkt_t *pkt = cp->metadata.pkt;
+	/* Identifier should match */
+	uint8_t pkt_id = ppp_pkt_get_id(pkt);
+	uint8_t pkt_length = ppp_pkt_get_length(pkt);
+
+	if (pkt_id != cp->cr_sent_identifier)
+	{
+		return -1; /* TODO: Fix error code*/
+	}
+
+	if (cp->cr_sent_size != pkt_length || memcmp(cp->cr_sent_opts,pkt->payload,pkt_length-sizeof(cp_hdr_t)))
+	{
+		return -1; /* TODO: Error code*/
+	}
+
+	cp->event = E_RCA;
+return 0; /*TODO: Fix output*/
+}
+
+/* Fix params for request */
+static int _handle_cp_nak(ppp_cp_t *cp)
+{
+	cp->negotiate_nak(cp->cp_options, &cp->metadata);
+	cp->event = E_RCN;
+	return 0; /*TODO: Fix output*/
+}
+
+static int _handle_cp_rej(ppp_cp_t *cp)
+{
+	/* Turn off every option that is not allowed by peer */
+	opt_metadata_t *opt_handler = cp->metadata.opts;
+	void *curr_opt = ppp_opts_get_head(opt_handler);
+	int num_opts = ppp_opts_get_num(opt_handler);
+
+	int curr_type;
+
+	for(int i=0;i<num_opts;i++)
+	{
+		curr_type = ppp_opt_get_tye(curr_opt);
+
+	}
+	l_lcp->event = E_RCJ;
+}
+
+#if 0
+
+static int _handle_cp_term_req(ppp_cp_t *cp, cp_pkt_t *pkt)
+{
+	cp->tr_identifier = cp_pkt->hdr->id;
+	cp->event = E_RTR;
+}
+
+static int _handle_cp_term_ack(ppp_cp_t *cp, cp_pkt_t *pkt)
+{
+	cp->event = E_RTA;
+}
+
+static int _handle_cp_code_rej(ppp_cp_t *cp, gnrc_pktsnip_t *pkt)
+{
+	cp->event = E_RXJm;
+}
+#endif
+
