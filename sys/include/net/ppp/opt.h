52612b688fc95c030d821aa1455fe8c231e5e242
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-03-23 11:59:28 +0100

    PPP: opt: Renamed opt_metadata_t to opt_list_t

--- sys/include/net/ppp/opt.h
+++ sys/include/net/ppp/opt.h
@@ -15,14 +15,14 @@ typedef struct __attribute__((packed)){
 	uint8_t length;
 } cp_opt_hdr_t;
 
-typedef struct opt_metadada_t
+typedef struct opt_list_t
 {
 	int num;
 	void *head;
 	void *current;
 
 	int _co;
-} opt_metadata_t;
+} opt_list_t;
 
 static inline int _get_num_opt(void *head_opt, uint16_t opts_length)
 {
@@ -47,48 +47,48 @@ static inline int _get_num_opt(void *head_opt, uint16_t opts_length)
 	
 	return num;
 }
-static inline int ppp_opts_init(opt_metadata_t *opt_metadata, cp_pkt_t *pkt)
+static inline int ppp_opts_init(opt_list_t *opt_list, cp_pkt_t *pkt)
 {
-	opt_metadata->head = opt_metadata->current = pkt->payload;
-	opt_metadata->_co = 0;
+	opt_list->head = opt_list->current = pkt->payload;
+	opt_list->_co = 0;
 	uint16_t pkt_length = ppp_pkt_get_length(pkt);
 
 	int num = _get_num_opt(pkt->payload, pkt_length-sizeof(cp_hdr_t));
 	if (num == -EBADMSG)
 		return -1;
 	
-	opt_metadata->num = num;
+	opt_list->num = num;
 	return 0;
 }
-static inline void *ppp_opts_get_head(opt_metadata_t *opt_metadata)
+static inline void *ppp_opts_get_head(opt_list_t *opt_list)
 {
-	return opt_metadata->head;
+	return opt_list->head;
 }
-static inline void ppp_opts_reset(opt_metadata_t *opt_metadata)
+static inline void ppp_opts_reset(opt_list_t *opt_list)
 {
-	opt_metadata->current = opt_metadata->head;
+	opt_list->current = opt_list->head;
 }
-static inline void *ppp_opts_next(opt_metadata_t *opt_metadata)
+static inline void *ppp_opts_next(opt_list_t *opt_list)
 {
-	void *current = opt_metadata->current;
+	void *current = opt_list->current;
 	uint8_t opt_size = *((uint8_t*)current+1);
 
-	if(opt_metadata->_co < opt_metadata->num-1)
+	if(opt_list->_co < opt_list->num-1)
 	{
-		opt_metadata->current = (void*) (((uint8_t*) current)+opt_size);
-		opt_metadata->_co += 1;
-		return opt_metadata->current; 
+		opt_list->current = (void*) (((uint8_t*) current)+opt_size);
+		opt_list->_co += 1;
+		return opt_list->current; 
 	}
 	return NULL;
 }
-static inline int ppp_opts_get_num(opt_metadata_t *opt_metadata)
+static inline int ppp_opts_get_num(opt_list_t *opt_list)
 {
-	return opt_metadata->num;
+	return opt_list->num;
 }
 
-static inline int ppp_opts_get_opt_num(opt_metadata_t *opt_metadata)
+static inline int ppp_opts_get_opt_num(opt_list_t *opt_list)
 {
-	return opt_metadata->_co;
+	return opt_list->_co;
 }
 
 static inline uint8_t ppp_opt_get_type(void *opt)
