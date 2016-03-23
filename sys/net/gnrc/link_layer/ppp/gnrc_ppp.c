0f97cb7540531b6f72dec4a35214f43a98011fd7
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-03-23 11:44:08 +0100

    Moved all fsm cp functions to gnrc_ppp

--- sys/net/gnrc/link_layer/ppp/gnrc_ppp.c
+++ sys/net/gnrc/link_layer/ppp/gnrc_ppp.c
@@ -280,3 +280,86 @@ kernel_pid_t gnrc_ppp_init(char *stack, int stacksize, char priority,
 }
 #endif
 #endif
+
+/*Send Control Protocol. Assumes the opt payload is loaded in HDLC Control Protocol Buffer. */
+static int send_cp(ppp_ctrl_prot_t  *cp, cp_pkt_t *pkt)
+{
+	ppp_dev_t *dev = cp->dev;
+
+	/* Set code, identifier, length*/
+	dev->_payload_buf[0] = code;
+	dev->_payload_buf[1] = identifier;
+	uint32_t length;
+	/* if size is not zero, the hdlc cp buffer was preloaded */
+	/*TODO: Change number to labels*/
+	uint16_t cursor;
+	/* Generate payload with corresponding options */
+	cursor = 0;
+	cp_opt_t *copt;
+	for(int i=0;i<l_lcp->_num_opt;i++)
+	{
+		copt = &(opt_stack->_opt_buf[i]);
+		*(dst+cursor) = copt->type;
+		*(dst+cursor+1) = copt->p_size+2;
+		for(int j=0;j<copt->opt_size;j++)
+		{
+			*(dst+cursor+2+i) = copt->payload[j];
+		}
+		cursor += copt->opt_size+2;
+	}
+	length =  opt_size+4;
+	dev->_payload_buf[2] = length & 0xFF00;
+	dev->_payload_buf[3] = length & 0x00FF;
+
+	/* Create pkt snip */
+	gnrc_pktsnip_t *pkt = gnrc_pktbuf_add(NULL, dev->_payload_buf, length, GNRC_NETTYPE_UNDEF);
+	if (pkt == NULL){
+		DEBUG("PPP: not enough space in pkt buffer");
+		return 0; /*TODO Fix*/
+	}
+	
+	/* Send pkt to ppp_send*/
+	ppp_send(dev, pkt);
+	return 0; /*TODO*/
+}
+
+/* Call functions depending on function flag*/
+static void _event_action(ppp_cp_t *cp) 
+{
+	uint8_t flags;
+	/* Reset link status */
+	cp->l_upper_msg = 0;
+	cp->l_lower_msg = 0;
+
+	flags = actions[cp->state][cp->event];
+
+	if(flags & F_TLU) _tlu(cp);
+	if(flags & F_TLD) _tld(cp);
+	if(flags & F_TLS) _tls(cp);
+	if(flags & F_TLF) _tlf(cp);
+	if(flags & F_IRC) _irc(cp);
+	if(flags & F_ZRC) _zrc(cp);
+	if(flags & F_SRC) _src(cp);
+	if(flags & F_SCA) _sca(cp);
+	if(flags & F_SCN) _scn(cp);
+	if(flags & F_STR) _str(cp);
+	if(flags & F_STA) _sta(cp);
+	if(flags & F_SCJ) _scj(cp);
+	if(flags & F_SER) _ser(cp);
+}
+
+static int fsm(ppp_cp_t *cp)
+{
+	int8_t next_state;
+
+	/* Call the appropiate functions for each state */
+	_event_action(cp);
+	/* Set next state */
+	next_state = state_trans[cp->state][cp->event];
+
+	/* Keep in same state if there's something wrong (RFC 1661) */
+	if(next_state != S_UNDEF){
+		cp->state = next_state;
+	}
+	return 0; /*TODO*/
+}
