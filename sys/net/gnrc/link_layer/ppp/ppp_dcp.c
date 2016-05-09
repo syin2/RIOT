96b69595004d6d37caaa7d149b733b5d59c7de62
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-05-09 15:08:11 +0200

    Automatic reconnect working

--- sys/net/gnrc/link_layer/ppp/ppp_dcp.c
+++ sys/net/gnrc/link_layer/ppp/ppp_dcp.c
@@ -5,6 +5,7 @@ int dcp_handler(struct ppp_protocol_t *protocol, uint8_t ppp_event, void *args)
 	msg_t *msg = &((dcp_t*) protocol)->msg;
 	msg_t *timer_msg = &((dcp_t*) protocol)->timer_msg;
 	xtimer_t *xtimer = &((dcp_t*) protocol)->xtimer;
+	dcp_t *dcp = (dcp_t*) protocol;
 	DEBUG("In DCP handler\n");
 	switch(ppp_event)
 	{
@@ -16,6 +17,8 @@ int dcp_handler(struct ppp_protocol_t *protocol, uint8_t ppp_event, void *args)
 			msg->type = PPPDEV_MSG_TYPE_EVENT;
 			msg->content.value = PPPDEV_LINK_DOWN_EVENT;
 			msg_send(msg, thread_getpid());
+			/*Remove timer*/
+			xtimer_remove(xtimer);
 			break;
 		case PPP_LINKUP:
 			DEBUG("Driver: PPP_LINKUP\n");
@@ -25,7 +28,7 @@ int dcp_handler(struct ppp_protocol_t *protocol, uint8_t ppp_event, void *args)
 
 			/*Start monitor*/
 			timer_msg->type = GNRC_PPPDEV_MSG_TYPE_EVENT;
-			timer_msg->content.value = (ID_LCP<<8) | PPP_MONITOR;		
+			timer_msg->content.value = (ID_PPPDEV<<8) | PPP_MONITOR;		
 			xtimer_set_msg(xtimer, 5000000, timer_msg, thread_getpid());
 			break;
 		case PPP_LINKDOWN:
@@ -35,10 +38,28 @@ int dcp_handler(struct ppp_protocol_t *protocol, uint8_t ppp_event, void *args)
 			msg_send(msg, thread_getpid());
 			break;
 		case PPP_MONITOR:
+			if(dcp->dead_counter)
+			{
+				msg->type = GNRC_PPPDEV_MSG_TYPE_EVENT;
+				msg->content.value = (ID_LCP<<8) | PPP_MONITOR;		
+				msg_send(msg, thread_getpid());
+
+				timer_msg->type = GNRC_PPPDEV_MSG_TYPE_EVENT;
+				timer_msg->content.value = (ID_PPPDEV<<8) | PPP_MONITOR;		
+				xtimer_set_msg(xtimer, 5000000, timer_msg, thread_getpid());
+				dcp->dead_counter -= 1;
+			}
+			else
+			{
+				msg->type = GNRC_PPPDEV_MSG_TYPE_EVENT;
+				msg->content.value = (ID_PPPDEV<<8) | PPP_UL_FINISHED;		
+				msg_send(msg, thread_getpid());
+				dcp->dead_counter = DCP_DEAD_COUNTER;
+			}
+			break;
+		case PPP_LINK_ALIVE:
 			DEBUG("Received ECHO request! Link working! :)\n");
-			timer_msg->type = GNRC_PPPDEV_MSG_TYPE_EVENT;
-			timer_msg->content.value = (ID_LCP<<8) | PPP_MONITOR;		
-			xtimer_set_msg(xtimer, 5000000, timer_msg, thread_getpid());
+			dcp->dead_counter = DCP_DEAD_COUNTER;
 			break;
 		default:
 			break;
@@ -51,5 +72,6 @@ int dcp_init(gnrc_pppdev_t *ppp_dev, ppp_protocol_t *dcp)
 	dcp->id = ID_PPPDEV;
 	((dcp_t*) dcp)->sent_id = 0;
 	((dcp_t*) dcp)->pppdev = ppp_dev;
+	((dcp_t*) dcp)->dead_counter = DCP_DEAD_COUNTER;
 	return 0;
 }
