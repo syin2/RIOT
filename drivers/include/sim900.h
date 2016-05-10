ed09c58202de80dd2cb83f06ebcd152911744e27
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-05-10 20:52:26 +0200

    Uncrustified driver

--- drivers/include/sim900.h
+++ drivers/include/sim900.h
@@ -23,17 +23,17 @@ extern "C" {
  * @brief	Enum of possible states of sim900 device
  */
 typedef enum {
-	AT_STATE_IDLE,
-	AT_STATE_CMD,
-	AT_STATE_RX,
+    AT_STATE_IDLE,
+    AT_STATE_CMD,
+    AT_STATE_RX,
 } dev_state_t;
 
 /**
  * @brief	Enum of possible states while receiving data
  */
-typedef enum{
-	PPP_RX_STARTED,
-	PPP_RX_IDLE
+typedef enum {
+    PPP_RX_STARTED,
+    PPP_RX_IDLE
 } ppp_rx_state_t;
 
 
@@ -42,36 +42,35 @@ typedef enum{
  * @extends pppdev_t
  */
 typedef struct sim900_t {
-	pppdev_t netdev; /**< extended pppdev structure */
-	uart_t uart; /**< UART port of device */
-	uint8_t rx_buf[SIM900_MAX_CMD_SIZE]; /**< rx buffer */
-	uint16_t rx_count; /**< number of bytes received from device */
-	uint16_t int_count; /**< same as rx_count, but as a temporal variable in interrupt context */
-	dev_state_t state; /**< state of device */
-	mutex_t out_mutex; /**< mutex for locking thread on write ops */
-	kernel_pid_t mac_pid; /**< PID of thread */
-	uint8_t _num_esc; /**< number of of escape strings in current AT command */
-	uint32_t _stream; /**< stream containing last 4 received AT characters */
-	uint8_t at_status; /**< flags of AT command content */
-	xtimer_t xtimer; /**< timer for handling timeouts */
-	void (*_cb)(struct sim900_t *dev); /**< callback when AT command finishes */
-	void (*_timer_cb)(struct sim900_t *dev); /**< callback when there's a timeout */
-	msg_t msg; /**< msg structure for sending events*/
-	ppp_rx_state_t ppp_rx_state; /**< rx state of device in data mode*/
-	uint8_t escape; /**< escape character (0x20 or 0x00)*/
-	uint16_t int_fcs; /**< fcs holder */
-	uint32_t tx_accm; /**< Async Control Character Map for transmission */
-	uint32_t rx_accm; /**< Async Control Character Map for reception */
-	uint8_t apn[SIM900_APN_SIZE]; /**< stores APN name */
-	uint8_t apn_len; /**< stores APN name */
+    pppdev_t netdev;                            /**< extended pppdev structure */
+    uart_t uart;                                /**< UART port of device */
+    uint8_t rx_buf[SIM900_MAX_CMD_SIZE];        /**< rx buffer */
+    uint16_t rx_count;                          /**< number of bytes received from device */
+    uint16_t int_count;                         /**< same as rx_count, but as a temporal variable in interrupt context */
+    dev_state_t state;                          /**< state of device */
+    mutex_t out_mutex;                          /**< mutex for locking thread on write ops */
+    kernel_pid_t mac_pid;                       /**< PID of thread */
+    uint8_t _num_esc;                           /**< number of of escape strings in current AT command */
+    uint32_t _stream;                           /**< stream containing last 4 received AT characters */
+    uint8_t at_status;                          /**< flags of AT command content */
+    xtimer_t xtimer;                            /**< timer for handling timeouts */
+    void (*_cb)(struct sim900_t *dev);          /**< callback when AT command finishes */
+    void (*_timer_cb)(struct sim900_t *dev);    /**< callback when there's a timeout */
+    msg_t msg;                                  /**< msg structure for sending events*/
+    ppp_rx_state_t ppp_rx_state;                /**< rx state of device in data mode*/
+    uint8_t escape;                             /**< escape character (0x20 or 0x00)*/
+    uint16_t int_fcs;                           /**< fcs holder */
+    uint32_t tx_accm;                           /**< Async Control Character Map for transmission */
+    uint32_t rx_accm;                           /**< Async Control Character Map for reception */
+    uint8_t apn[SIM900_APN_SIZE];               /**< stores APN name */
+    uint8_t apn_len;                            /**< stores APN name */
 } sim900_t;
 
 /**
  * @brief	Struct containing the needed peripheral configuration
  */
-typedef struct sim900_params_t
-{
-	uart_t uart; /**< UART port of device */
+typedef struct sim900_params_t {
+    uart_t uart; /**< UART port of device */
 } sim900_params_t;
 
 /**
