486081a442dd24b7887333d108cf189867bc242e
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-05-27 14:35:14 +0200

    Added apn settings in cli

--- examples/sim900/main.c
+++ examples/sim900/main.c
@@ -17,7 +17,7 @@ static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 int ppp_cmd(int argc, char **argv)
 {
 	if(argc < 2) {
-		printf("usage: %s [set|get|status|dial_up]\n", argv[0]);
+		printf("usage: %s [apn|tunnel|status|dial_up]\n", argv[0]);
 		return 1;
 	}
 
@@ -25,12 +25,32 @@ int ppp_cmd(int argc, char **argv)
 	{
 		if (argc < 3)
 		{
-			printf("usage: %s, dial_up <if>\n", argv[0]);
+			printf("usage: %s dial_up <if>\n", argv[0]);
 			return 1;
 		}
 		msg_t msg;
 		printf("Dialing up PPP\n");
 		gnrc_ppp_dial_up(&msg, atoi(argv[2]));
+	}
+	else if(strcmp(argv[1],"apn") == 0)
+	{
+		if(argc < 4)
+		{
+			printf("usage: %s apn <if> <apn_address> [<apn_username> [<apn_pass>]]", argv[0]);
+			return 1;
+		}
+		gnrc_netapi_set(atoi(argv[2]), NETOPT_APN_NAME, 0, argv[3], strlen(argv[3]));
+		printf("setting apn to %s\n", argv[3]);
+		if(argc > 4)
+		{
+			gnrc_netapi_set(netifs[1], NETOPT_APN_USER, 0, argv[4], strlen(argv[4]));;
+			printf("setting apn username to %s\n", argv[4]);
+		}
+		if(argc > 5)
+		{
+			gnrc_netapi_set(netifs[1], NETOPT_APN_PASS, 0, argv[5], strlen(argv[5]));;
+			printf("setting apn password to %s\n", argv[5]);
+		}
 	}
 	return 0;
 }
@@ -46,7 +66,6 @@ int main(void)
 
 	DEBUG("Number of interfaces: %i\n", gnrc_netif_get(netifs));
 
-	char apn[] ="mmsbouygtel.com";
 	ipv4_addr_t tunnel_addr;
 	uint16_t port = 9876;
 	tunnel_addr.u8[0] = 51;
@@ -56,11 +75,8 @@ int main(void)
 
 	char apn_user[] = "test";
 	char apn_pass[] = "test";
-	gnrc_netapi_set(netifs[1], NETOPT_APN_NAME, 0, apn, sizeof(apn)-1);
 	gnrc_netapi_set(netifs[1], NETOPT_TUNNEL_IPV4_ADDRESS, 0, (void*) &tunnel_addr , sizeof(ipv4_addr_t));
 	gnrc_netapi_set(netifs[1], NETOPT_TUNNEL_UDP_PORT, 0, (void*) &port , sizeof(uint16_t));
-	gnrc_netapi_set(netifs[1], NETOPT_APN_USER, 0, apn_user, sizeof(apn_user)-1);
-	gnrc_netapi_set(netifs[1], NETOPT_APN_PASS, 0, apn_pass, sizeof(apn_pass)-1);
 
 #if ENABLE_SHELL
 	char line_buf[SHELL_DEFAULT_BUFSIZE];
