bfb9ad8b2e58670f7cc3a088e23024348b5ce307
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-06-13 15:27:58 +0200

    ppp_hdr: uncrustified

--- sys/include/net/ppptype.h
+++ sys/include/net/ppptype.h
@@ -29,13 +29,13 @@ extern "C" {
 #endif
 
 
-#define PPPTYPE_IPV6       (0x0057)    /**< IPv6 packet in PPP*/
-#define PPPTYPE_IPV4       (0x0021) /**< IPv4 packet in PPP */
-#define PPPTYPE_NCP_IPV6       (0x8057)    /**< NCP packet for IPV6*/
-#define PPPTYPE_NCP_IPV4       (0x8021)    /**< NCP packet for IPv4*/
-#define PPPTYPE_LCP       (0xC021)    /**< LCP packet  */
-#define PPPTYPE_PAP       (0xC023)    /**< PAP packet  */
-#define PPPTYPE_UNKNOWN       (0x8001)    /**<Unknown  packet (Unused protocol number according to IANA) */
+#define PPPTYPE_IPV6       (0x0057)     /**< IPv6 packet in PPP*/
+#define PPPTYPE_IPV4       (0x0021)     /**< IPv4 packet in PPP */
+#define PPPTYPE_NCP_IPV6       (0x8057) /**< NCP packet for IPV6*/
+#define PPPTYPE_NCP_IPV4       (0x8021) /**< NCP packet for IPv4*/
+#define PPPTYPE_LCP       (0xC021)      /**< LCP packet  */
+#define PPPTYPE_PAP       (0xC023)      /**< PAP packet  */
+#define PPPTYPE_UNKNOWN       (0x8001)  /**<Unknown  packet (Unused protocol number according to IANA) */
 
 #ifdef __cplusplus
 }
