813dec1d727ce0cd33ec2b8df94e2a5045302a74
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-10-05 15:07:26 -0300

    add isr_flags to sim900 driver

--- drivers/include/sim900.h
+++ drivers/include/sim900.h
@@ -67,6 +67,7 @@ typedef struct sim900_t {
     uint8_t apn[SIM900_APN_SIZE];               /**< stores APN name */
     uint8_t apn_len;                            /**< stores APN name */
     uint8_t mac_addr[DUMMY_ADDR_LEN];           /**< Dummy MAC holder*/
+	uint8_t isr_flags;
 } sim900_t;
 
 /**
