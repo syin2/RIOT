3ed9ae59971b82bb211d9a5975b7245823e4e5d5
José Ignacio Alamos Aste <jialamos@uc.cl>
2017-01-15 17:19:05 -0300

    fixed getters and setters pointers

--- drivers/netdev2_ppp/netdev2_ppp.c
+++ drivers/netdev2_ppp/netdev2_ppp.c
@@ -22,68 +22,67 @@
 #include <errno.h>
 
 #include "net/netdev2.h"
-#include "net/eui64.h"
-#include "net/ethernet.h"
+#include "net/netdev2/ppp.h"
 
 #define ENABLE_DEBUG (0)
 #include "debug.h"
 
-int netdev2_ppp_get(netdev2_t *dev, netopt_t opt, void *value, size_t max_len)
+int netdev2_ppp_get(netdev2_ppp_t *dev, netopt_t opt, void *value, size_t max_len)
 {
-	netdev2_ppp_t *pppdev = (netdev2_ppp_t*) dev;
     int res;
+	netdev2_t *netdev = (netdev2_t*) dev;
     switch (opt) {
         case NETOPT_PPP_LCP_STATE:
-            *((uint8_t *) value) = pppdev->lcp->state;
+            *((uint8_t *) value) = ((ppp_protocol_t*) &dev->lcp)->state;
             res = 0;
             break;
         case NETOPT_PPP_AUTH_STATE:
-            *((uint8_t *) value) = pppdev->pap->state;
+            *((uint8_t *) value) = ((ppp_protocol_t*) &dev->pap)->state;
             res = 0;
             break;
         case NETOPT_PPP_IPCP_STATE:
-            *((uint8_t *) value) = pppdev->ipcp->state;
+            *((uint8_t *) value) = ((ppp_protocol_t*) &dev->ipcp)->state;
             res = 0;
             break;
         case NETOPT_PPP_IS_IPV6_READY:
-            res = pppdev->ipv4->state == PROTOCOL_UP;
+            res = ((ppp_protocol_t*) &dev->ipv4)->state == PROTOCOL_UP;
             *((uint8_t *) value) = res;
             break;
         case NETOPT_IS_PPP_IF:
             res = 1;
             break;
         default:
-            res = dev->driver->get(dev, opt, value, value_len);
+            res = netdev->driver->get(netdev, opt, value, max_len);
             break;
     }
     return res;
 }
 
-int netdev2_ppp_set(netdev2_t *dev, netopt_t opt, void *value, size_t value_len)
+int netdev2_ppp_set(netdev2_ppp_t *dev, netopt_t opt, void *value, size_t value_len)
 {
-	netdev2_ppp_t *pppdev = (netdev2_ppp_t*) dev;
     int res;
+	netdev2_t *netdev = (netdev2_t*) dev;
     switch (opt) {
         case NETOPT_TUNNEL_IPV4_ADDRESS:
-            ((ppp_ipv4_t *) pppdev->ipv4)->tunnel_addr = *((ipv4_addr_t *) value);
+            dev->ipv4.tunnel_addr = *((ipv4_addr_t *) value);
             res = 0;
             break;
         case NETOPT_TUNNEL_UDP_PORT:
-            ((ppp_ipv4_t *) pppdev->ipv4)->tunnel_port = *((uint16_t *) value);
+            dev->ipv4.tunnel_port = *((uint16_t *) value);
             res = 0;
             break;
         case NETOPT_APN_USER:
-            memcpy(((pap_t *) pppdev->pap)->username, value, value_len);
-            ((pap_t *) pppdev->pap)->user_size = value_len;
+            memcpy(dev->pap.username, value, value_len);
+            dev->pap.user_size = value_len;
             res = 0;
             break;
         case NETOPT_APN_PASS:
-            memcpy(((pap_t *) pppdev->pap)->password, value, value_len);
-            ((pap_t *) pppdev->pap)->pass_size = value_len;
+            memcpy(dev->pap.password, value, value_len);
+            dev->pap.pass_size = value_len;
             res = 0;
             break;
         default:
-            res = dev->driver->set(dev, opt, value, value_len);
+            res = netdev->driver->set(netdev, opt, value, value_len);
             break;
     }
     return res;
