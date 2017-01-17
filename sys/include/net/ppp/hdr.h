6cfa58db86af66d6eb6096654e4a4d2036934628
José Ignacio Alamos Aste <jialamos@uc.cl>
2017-01-17 00:00:19 -0300

    fixed <<<<< from merge

--- sys/include/net/ppp/hdr.h
+++ sys/include/net/ppp/hdr.h
@@ -48,7 +48,6 @@ extern "C" {
  * +-+-+-+-+
  *
  *
-<<<<<<< 274d77e6a5d6ef0e3500f996a7e168f41b0d6563
  * @see <a href="https://tools.ietf.org/html/rfc1661#section-5">
  *          RFC 1661, section 5
  *      </a>
@@ -60,103 +59,9 @@ typedef struct __attribute__((packed)){
     network_uint16_t length;    /**< Length of PPP packet including payload*/
 } ppp_hdr_t;
 
-=======
- */
-/*  PPP pkt header struct */
-typedef struct __attribute__((packed)){
-    uint8_t code;
-    uint8_t id;
-    network_uint16_t length;
-} ppp_hdr_t;
-
-
-
-/**
- * @brief    Get code field of PPP packet
- *
- * @param[in] hdr    PPP header
- *
- * @return    code of PPP header
- */
-static inline uint8_t ppp_hdr_get_code(ppp_hdr_t *hdr)
-{
-    return hdr->code;
-}
-
-
-/**
- * @brief    Set code field of PPP header
- *
- * @param[in] hdr    PPP header
- * @param[in] code    code to be set
- *
- */
-static inline void ppp_hdr_set_code(ppp_hdr_t *hdr, uint8_t code)
-{
-    hdr->code = code;
-}
-
-
-/**
- * @brief    Get id field of PPP header
- *
- * @param[in] hdr    PPP header
- *
- * @return    id of PPP header
- */
-static inline uint8_t ppp_hdr_get_id(ppp_hdr_t *hdr)
-{
-    return hdr->id;
-}
-
-
-/**
- * @brief    Set id field of PPP header
- *
- * @param[in] hdr    PPP header
- * @param[in] id    id to be set
- *
- */
-static inline void ppp_hdr_set_id(ppp_hdr_t *hdr, uint8_t id)
-{
-    hdr->id = id;
-}
-
-
-/**
- * @brief    Get length field of PPP header
- *
- * @param[in] hdr    PPP header
- *
- * @return    length of PPP header
- */
-static inline uint16_t ppp_hdr_get_length(ppp_hdr_t *hdr)
-{
-    return byteorder_ntohs(hdr->length);
-}
-
-
-
-/**
- * @brief    Set length field of PPP header
- *
- * @param[in] hdr    PPP header
- * @param[in] length    length to be set
- *
- */
-static inline void ppp_hdr_set_length(ppp_hdr_t *hdr, uint16_t length)
-{
-    hdr->length = byteorder_htons(length);
-}
-
-
->>>>>>> PPP: hdr: added PPP header definition and helpers
 #ifdef __cplusplus
 }
 #endif
 
 #endif /* PPP_HDR_H_ */
-<<<<<<< 274d77e6a5d6ef0e3500f996a7e168f41b0d6563
 /** @} */
-=======
->>>>>>> PPP: hdr: added PPP header definition and helpers
