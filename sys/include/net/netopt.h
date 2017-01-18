08900feb64f670724f72c330692baa897b9e31ae
José Alamos <jialamos@uc.cl>
2017-01-18 13:04:05 -0300

    documented netopt

--- sys/include/net/netopt.h
+++ sys/include/net/netopt.h
@@ -291,7 +291,14 @@ typedef enum {
      * option is the same as the pointer.
      */
     NETOPT_PPP_IS_IPV6_READY,
+
+    /**
+     * @brief triggers a device driver dial up or hang up.
+     *
+     * Triggers dial up if enabled. Hang up if not. 
+     */
     NETOPT_DIAL_UP,
+
     /**
      * @brief en/disable encryption.
      */
